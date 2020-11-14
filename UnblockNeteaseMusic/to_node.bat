d2cc36d3f45a75581b8c91ef2fcef667091bb731
rocsun <978614060@qq.com>
2020-11-14 09:49:29 +0800

    recommit

--- UnblockNeteaseMusic/to_node.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-For /f "skip=1 tokens=2 delims=[" %%a in ('ping  music.163.com') do (
-        For /f "tokens=1 delims=]" %%b in ("%%a") do (
-                Set ip=%%b
-        )
-)
-cd %~dp0
-node\node.exe app.js -p 2333 -f %ip%
